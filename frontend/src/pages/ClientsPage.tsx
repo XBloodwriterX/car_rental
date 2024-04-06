15c91a77370a0c30d359a4218e538e4208fd79fd
XBloodwriterX <kiyashko.timofey@gmail.com>
2024-04-06 15:02:27 +0200

    Add ClientsPage with styles

--- frontend/src/pages/ClientsPage.tsx
+++ frontend/src/pages/ClientsPage.tsx
@@ -1,18 +1,40 @@
 import React, {useEffect, useState} from 'react';
 import {Button} from "reactstrap";
+import {OutputProps} from "../types";
+import axios from "axios";
 
 const ClientsPage = () => {
-    let [count, setCount] = useState(0)
-
+    let [details, setDetails] = useState<OutputProps[]>()
 
     useEffect(() => {
-    console.log('effect')
+        axios.get<OutputProps[]>('http://localhost:8000/clients/')
+            .then(res => {
+                console.log('Got data '+res.data)
+                setDetails(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            });
     }, []);
+
+
     return (
         <main>
             <h1>All Clients:</h1>
             <hr/>
-            <Button onChange={() => setCount(count+1)}></Button>
+            {details?.map((data, id) => (
+                <div key={id}>
+                    <h1>{data.id}. {data.first_name} {data.last_name}</h1>
+                    <p>Age: {data.age}</p>
+                    <p>PESEL: {data.PESEL}</p>
+                    <p>Telephone number: {data.telephone_number}</p>
+                    <p>Email: <a href={"mailto:"+data.email}>{data.email}</a></p>
+                    <p>Locality: {data.locality}</p>
+                    <p>Street: {data.street}</p>
+                    <p>Zip code: {data.zip_code}</p>
+                    <p>Notes: {data.notes}</p>
+                </div>
+            ))}
         </main>
     );
 };
